2c239fe06fbc28b593bef041d5addbd964b44693
frizalaptop <frizamuhammadcrb1234@gmail.com>
2025-09-26 19:56:52 +0700

    instalasi laravel + adminlte

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,3 @@
-import './bootstrap';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'admin-lte/dist/js/adminlte.min.js';
+import './axios';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'admin-lte/dist/js/adminlte.min.js';
