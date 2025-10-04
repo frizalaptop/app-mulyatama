c6c4dc627988aadc8ae06a46f4960af9bddb3978
Mahmud <machmudseo@gmail.com>
2025-10-04 22:12:25 +0700

    ganti nama kolom db user_profile

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -10,17 +10,17 @@ class Profile extends Model
     protected $table = 'user_profile';
 
     protected $fillable = [
-        'pf_iduser',
-        'pf_company',
-        'pf_wa',
-        'pf_telegram',
-        'pf_address',
-        'pf_photo',
+        'user_id',
+        'perusahaan',
+        'whatsapp',
+        'telegram',
+        'alamat',
+        'photo',
     ];
 
     // relasi balik ke user
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'pf_iduser', 'id');
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 }
