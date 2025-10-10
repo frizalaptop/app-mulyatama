46b18b9a11b2dcbbaf956633f47c02daa39129c1
frizalaptop <frizamuhammadcrb1234@gmail.com>
2025-10-10 11:22:58 +0700

    add: readme.php

---
+++ readme.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * README
+ * File ini hanya berisi dokumentasi atau catatan penting terkait proyek ini.
+ * 
+ * << Route >>
+ * routes\web.php
+ * 
+ * << Config >>
+ * config\app.php
+ * config\database.php
+ * config\adminlte.php
+ * 
+ * << Event >>
+ * app\Events\UserSensitiveDataChanged.php
+ * 
+ * << Controller Admin >>
+ * app\Http\Controllers\Admin\DashboardController.php
+ * app\Http\Controllers\Admin\StatistikController.php
+ * app\Http\Controllers\Admin\User\UserListController.php
+ * 
+ * << Controller Klien >>
+ * app\Http\Controllers\Klien\DashboardController.php
+ * 
+ * << Controller Global >>
+ * app\Http\Controllers\HomeController.php
+ * 
+ * << Form Request Validasi >>
+ * app\Http\Requests\AddUserRequest.php
+ * app\Http\Requests\UpdateUserRequest.php
+ * 
+ * << Listener >>
+ * app\Listeners\LogUserSensitiveDataChange.php
+ * 
+ * << Model >>
+ * app\Models\Profile.php
+ * app\Models\User.php
+ * 
+ * << Repository >>
+ * app\Repositories\UserRepository.php
+ * app\Repositories\ProfileRepository.php
+ * app\Repositories\RoleRepository.php
+ * 
+ * << Service >>
+ * app\Services\UserListService.php
+ * app\Services\StatistikService.php
+ * 
+ * << Trait (Helper) >>
+ * app\Traits\HandlersException.php
+ * app\Traits\ServiceLogger.php
+ * 
+ * << Javascript >>
+ * public\build\assets\user-list.min.js
+ * public\build\assets\custom_table.min.js
+ * public\build\assets\custom_format.min.js
+ * public\build\assets\custom_form.min.js
+ * 
+ * << CSS >>
+ * public\build\assets\custom.min.css
+ * 
+ * << View >>
+ * resources\views\layouts\app.blade.php (Main Layout)
+ * resources\views\user\user-list.blade.php
+ * resources\views\user\user-profile.blade.php
+ * resources\views\errors (Error Pages)
+ * 
+ * << Migration >>
+ * database\migrations
+ * 
+ * << Seeder >>
+ * database\seeders
+ * 
+ * << File Bahasa >>
+ * lang
+ * 
+ * 
+ */
