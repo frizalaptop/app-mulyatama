36686375051894d5cfbcc0dd6ba3c25026469040
frizapc <frizamuhammad99@gmail.com>
2025-10-13 18:47:53 +0700

    add update profil form request

---
+++ app/Http/Requests/UpdateProfilAkunRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\Rule;
+
+class UpdateProfilAkunRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:100|min:3',
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique('users')->ignore($this->userId),
+            ],
+            'password' => ['nullable', 'string', 'confirmed', Password::min(8)
+                    ->uncompromised()
+            ],
+        ];
+    }
+}
---
+++ app/Http/Requests/UpdateProfilInfoRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProfilInfoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'perusahaan' => 'nullable|string|max:255',
+            'whatsapp' => 'nullable|string|max:15',
+            'telegram' => 'nullable|string|max:255',
+            'alamat' => 'nullable|string',
+        ];
+    }
+}
