915253c48ef8bd22ed67059ef1966f9471b06375
frizapc <frizamuhammad99@gmail.com>
2025-10-28 19:50:24 +0700

    add: billboard-list view

---
+++ resources/views/billboard/billboard-list.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@push('css')
+    <link rel="stylesheet" href="{{ asset('/vendor/datatables/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/vendor/datatables-plugins/responsive/css/responsive.bootstrap4.min.css') }}">   
+@endpush
+
+
+
+@section('content')
+
+	<div class="row">
+		<div class="col-12">
+			<x-adminlte-card theme="light">
+				<!-- Table -->
+				<table id="tableBillboardList" class="table table-bordered table-hover dataTable dtr-inline"></table>
+			</x-adminlte-card>
+		</div>
+	</div>
+
+@stop
+
+@push('js')
+    <script>
+        // Definisi route endpoint yang dibutuhkan
+        window.routes = {
+            dataTable: "{{ route('billboard.list.tabel') }}",
+        };
+    </script>
+    <script src="{{ asset('/vendor/jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('/vendor/jquery-validation/localization/messages_id.min.js') }}"></script>
+    <script src="{{ asset('/vendor/datatables/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('/vendor/datatables/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('/vendor/datatables-plugins/responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('/vendor/datatables-plugins/responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('/vendor/datatables-plugins/buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('/vendor/datatables-plugins/buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('/vendor/datatables-plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('page/custom_format.min.js') }}"></script>
+    <script src="{{ asset('page/custom_form.min.js') }}"></script>
+    <script src="{{ asset('page/custom_table.min.js') }}"></script>
+    <script src="{{ asset('page/billboard-list.min.js') }}"></script>
+@endpush
