e640156e3f2d7aa1722fd4874303b1668c1fd680
frizapc <frizamuhammad99@gmail.com>
2025-10-28 19:54:17 +0700

    add: billboard model

---
+++ app/Models/Billboard.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Billboard extends Model
+{
+    // tabel database
+    protected $table = 'billboards';
+
+    // kolom yang dapat diisi
+    protected $fillable = [
+        'judul',
+        'area',
+        'lokasi',
+        'status',
+        'aktif',
+        'keterangan',
+        'jenis',
+        'lebar',
+        'panjang',
+        'unit',
+        'latitude',
+        'longitude',
+        'gambar',
+        'admin_buat',
+        'admin_ubah',
+    ];
+}
