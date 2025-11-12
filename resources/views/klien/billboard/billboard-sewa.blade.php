699a27540771d8927f755f22e98009dab368c358
frizapc <frizamuhammad99@gmail.com>
2025-11-12 21:29:47 +0700

    add: billboard sewa page

---
+++ resources/views/klien/billboard/billboard-sewa.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@push('css')
+    <link rel="stylesheet" href="{{ asset('/vendor/datatables/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/vendor/datatables-plugins/responsive/css/responsive.bootstrap4.min.css') }}">   
+@endpush
+
+
+
+@section('content')
+
+    <!-- Table -->
+	<div class="row">
+		<div class="col-12">
+			<x-adminlte-card theme="light">
+				<table id="tableBillboardSewa" class="table table-bordered table-hover dataTable dtr-inline"></table>
+			</x-adminlte-card>
+		</div>
+	</div>
+
+@stop
+
+@push('js')
+    <script>
+        // Definisi route endpoint yang dibutuhkan
+        window.routes = {
+            dataTable: "{{ route('klien.billboard.sewa.tabel') }}",
+            opsiFilter: "{{ route('klien.billboard.sewa.opsi.filter') }}",
+        };
+    </script>
+    <script src="{{ asset('/vendor/datatables/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('/vendor/datatables/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('/vendor/datatables-plugins/responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('/vendor/datatables-plugins/responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('/vendor/datatables-plugins/buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('/vendor/datatables-plugins/buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('/vendor/datatables-plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('page/custom_format.min.js') }}"></script>
+    <script src="{{ asset('page/custom_table.min.js') }}"></script>
+    <script src="{{ asset('page/klien/billboard-sewa.min.js') }}"></script>
+@endpush
