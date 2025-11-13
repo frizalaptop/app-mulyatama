a5242aebdce19cbdca249e38548bd9dc9d94ac91
frizapc <frizamuhammad99@gmail.com>
2025-11-13 15:15:51 +0700

    add: log for command process

--- app/Traits/ServiceLogger.php
+++ app/Traits/ServiceLogger.php
@@ -31,7 +31,7 @@ trait ServiceLogger
      * @param Throwable $e base interface untuk object apapun yang dapat di throw melalui throw statement
      * @return void
      */
-    protected function logException(Throwable $e): void
+    protected function logExceptionHttp(Throwable $e): void
     {
         Log::channel('daily')->error('Unhandled exception: ' . $e->getMessage(), [
             'exception' => get_class($e),
@@ -40,5 +40,38 @@ trait ServiceLogger
             'url' => request()->fullUrl() ?? null,
             'user_id' => auth()->user()->id ?? 'guest',
         ]);
+    }
+
+    /**
+     * Log keberhasilan eksekusi command Artisan.
+     * @param string $commandName nama perintah artisan
+     * @param string $message pesan hasil proses
+     * @param array $context (opsional) konteks perubahan seperti jumlah data yang berubah
+     * @return void
+     */
+    protected function logSuccessCommand(string $commandName, string $message = '', array $context = []): void
+    {
+        Log::channel('daily')->info("[Command: {$commandName}] Sukses dijalankan.", array_merge([
+            'message' => $message,
+            'time' => now()->toDateTimeString(),
+        ], $context));
+    }
+
+    /**
+     * Log kegagalan eksekusi command Artisan
+     * @param string $commandName nama perintah artisan
+     * @param \Throwable $e instance Throwable
+     * @param array $context (opsional) konteks perubahan seperti jumlah data yang berubah
+     * @return void
+     */
+    protected function logExceptionCommand(string $commandName, Throwable $e, array $context = []): void
+    {
+        Log::channel('daily')->error("[Command: {$commandName}] Gagal dijalankan!", array_merge([
+            'error' => $e->getMessage(),
+            'exception' => get_class($e),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'time' => now()->toDateTimeString(),
+        ], $context));
     }
 }
