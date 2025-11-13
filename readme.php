e32525650001fc0baaf7b538ae3ebb3e7451e5fa
frizapc <frizamuhammad99@gmail.com>
2025-11-13 15:36:38 +0700

    update: readme.php

--- readme.php
+++ readme.php
@@ -7,6 +7,12 @@
  * << Route >>
  * routes\web.php
  * 
+ * << Task Scheduling >>
+ * routes\console.php
+ * 
+ * << Custom Artisan Command >>
+ * app\Console\Commands
+ * 
  * << Config >>
  * config\app.php
  * config\database.php
@@ -16,19 +22,29 @@
  * app\Events\UserSensitiveDataChanged.php
  * 
  * << Controller Admin >>
+ * app\Http\Controllers\Admin\Billboard\BillboardController.php
+ * app\Http\Controllers\Admin\Billboard\BillboardSewaController.php
+ * app\Http\Controllers\Admin\User\UserListController.php
  * app\Http\Controllers\Admin\DashboardController.php
  * app\Http\Controllers\Admin\StatistikController.php
- * app\Http\Controllers\Admin\User\UserListController.php
  * 
  * << Controller Klien >>
+ * app\Http\Controllers\Klien\Billboard\BillboardController.php
+ * app\Http\Controllers\Klien\Billboard\BillboardSewaController.php
  * app\Http\Controllers\Klien\DashboardController.php
  * 
+ * << Controller Helpers >>
+ * app\Http\Controllers\Helpers\ControllerHelpers.php
+ * 
  * << Controller Global >>
- * app\Http\Controllers\HomeController.php
+ * app\Http\Controllers\ProfilController.php
  * 
  * << Form Request Validasi >>
  * app\Http\Requests\AddUserRequest.php
  * app\Http\Requests\UpdateUserRequest.php
+ * app\Http\Requests\UpdateProfilAkunRequest.php
+ * app\Http\Requests\UpdateProfilInfoRequest.php
+ * app\Http\Requests\AddUpdateBillboardRequest.php
  * 
  * << Listener >>
  * app\Listeners\LogUserSensitiveDataChange.php
@@ -36,31 +52,43 @@
  * << Model >>
  * app\Models\Profile.php
  * app\Models\User.php
+ * app\Models\Billboard.php
+ * app\Models\BillboardSewa.php
  * 
  * << Trait (Helper) >>
  * app\Traits\HandlersException.php
  * app\Traits\ServiceLogger.php
  * 
  * << Javascript >>
- * public\build\assets\user-list.min.js
- * public\build\assets\custom_table.min.js
- * public\build\assets\custom_format.min.js
- * public\build\assets\custom_form.min.js
+ * public\page\beranda.min.js
+ * public\page\custom_form.min.js
+ * public\page\custom_format.min.js
+ * public\page\custom_table.min.js
+ * public\page\profil.min.js
+ * public\page\admin\billboard-list.min.js
+ * public\page\admin\billboard-sewa.min.js
+ * public\page\admin\user-list.min.js
+ * public\page\klien\billboard-list.min.js
+ * public\page\klien\billboard-sewa.min.js
  * 
  * << CSS >>
- * public\build\assets\custom.min.css
+ * public\page\custom.min.css
  * 
  * << View >>
  * resources\views\layouts\app.blade.php (Main Layout)
- * resources\views\user\user-list.blade.php
- * resources\views\user\user-profile.blade.php
+ * resources\views\admin
+ * resources\views\klien
  * resources\views\errors (Error Pages)
+ * resources\views\profil.blade.php
  * 
  * << Migration >>
  * database\migrations
  * 
- * << Seeder >>
+ * << Seeder Database >>
  * database\seeders
+ * 
+ * << Factory Database >>
+ * database\factories
  * 
  * << File Bahasa >>
  * lang
