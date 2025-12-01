3626dbebf18387906dec451b357ff9c7786739ab
frizapc <frizamuhammad99@gmail.com>
2025-12-01 09:56:45 +0700

    chore(command): delete dependency not needed

--- app/Console/Commands/UpdateStatusBillboard.php
+++ app/Console/Commands/UpdateStatusBillboard.php
@@ -6,7 +6,6 @@ use App\Traits\ServiceLogger;
 use Illuminate\Console\Command;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 
 
