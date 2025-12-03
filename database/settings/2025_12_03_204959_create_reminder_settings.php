f878785151aa13a920268d4f1896acb0249a2de4
frizapc <frizamuhammad99@gmail.com>
2025-12-03 22:00:35 +0700

    feat(settings): add default value for reminder settings

---
+++ database/settings/2025_12_03_204959_create_reminder_settings.php
@@ -0,0 +1,18 @@
+<?php
+
+use Spatie\LaravelSettings\Migrations\SettingsMigration;
+
+return new class extends SettingsMigration
+{
+    public function up(): void
+    {
+        $this->migrator->add('reminder.Klien', [30, 14, 7, 1]);
+        $this->migrator->add('reminder.Admin', [30, 14, 7, 1]);
+    }
+
+    public function down(): void
+    {
+        $this->migrator->delete('reminder.klien');
+        $this->migrator->delete('reminder.admin');
+    }
+};
